3490a61653c37e87d75493036c40b38a9a1990ed
Harasaki Yoshika <nya.nya.yoska@gmail.com>
2018-06-07 19:49:20 +0900

    Update main.py

--- camera/main.py
+++ camera/main.py
@@ -4,7 +4,7 @@ from flask import Flask, render_template, Response
 # from processor.motion_detector import MotionDetector as VideoCamera
 # from processor.qr_detector import QRDetector as VideoCamera
 # from processor.face_detector import FaceDetector as VideoCamera
- from processor.person_detector import PersonDetector as VideoCamera
+from processor.person_detector import PersonDetector as VideoCamera
 
 import time
 import threading
